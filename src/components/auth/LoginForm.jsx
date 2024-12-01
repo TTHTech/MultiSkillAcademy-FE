d498013faaab242ae9c5a6927959fa51731e2757
Hoài Thanh <138654620+TTHTech@users.noreply.github.com>
2024-12-01 21:59:24 +0700

    commit

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -16,7 +16,7 @@ const LoginForm = () => {
     e.preventDefault();
 
     try {
-      const response = await axios.post("http://localhost:8080/api/auth/login", {
+      const response = await axios.post("https://educoresystem-1.onrender.com/api/auth/login", {
         username,
         password,
       });
