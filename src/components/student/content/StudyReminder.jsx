577bfc2c1d9c0fc57737386452a8acdb0f5284c0
Hanh Phuc Cong <hanhphucong28122003@gmail.com>
2025-02-14 20:11:41 +0700

    Cap nhat

--- src/components/student/content/StudyReminder.jsx
+++ src/components/student/content/StudyReminder.jsx
@@ -134,7 +134,10 @@ const StudyReminderList = () => {
             <h3 className="text-3xl font-semibold text-center mb-6">
               Tạo nhắc nhở học tập
             </h3>
-            <CreateStudyReminder closeModal={() => setShowCreateModal(false)} />
+            <CreateStudyReminder
+              closeModal={() => setShowCreateModal(false)}
+              triggerRefresh={triggerRefresh}
+            />
             <button
               onClick={() => setShowCreateModal(false)}
               className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
@@ -161,6 +164,7 @@ const StudyReminderList = () => {
               Data={selectedReminder}
               closeModal={() => setShowUpdateModal(false)}
               onUpdate={handleUpdate}
+              triggerRefresh={triggerRefresh}
             />
           </div>
         </div>
